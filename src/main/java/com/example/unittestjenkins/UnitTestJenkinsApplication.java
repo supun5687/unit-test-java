f214e4407b57410a9443c2180ebd0afb9e5582c2
supun5687 <absupuns95@gmail.com>
2023-06-04 14:50:04 +0530

    Initial commit

---
+++ src/main/java/com/example/unittestjenkins/UnitTestJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.unittestjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestJenkinsApplication.class, args);
+	}
+
+}
